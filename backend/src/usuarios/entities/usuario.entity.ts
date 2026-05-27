a98c97215a2280aaa549969f34e9608f9589f1fb
Mateus Nasc <105395333+Mateus-Nasc@users.noreply.github.com>
2026-05-26 23:46:38 -0300

    feat: implementando entidade usuarios

--- backend/src/usuarios/entities/usuario.entity.ts
+++ backend/src/usuarios/entities/usuario.entity.ts
@@ -1,1 +1,22 @@
-export class Usuario {}
+import { Exclude } from 'class-transformer';
+import { Tarefa } from 'src/tarefas/entities/tarefa.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('usuarios')
+export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 60 })
+  nome: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  @Exclude()
+  senha: string;
+
+  @OneToMany(() => Tarefa, (tarefa) => tarefa.usuario)
+  tarefas: Tarefa[];
+}

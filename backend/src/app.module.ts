03bd16b2f12d4759120d0239337c7fac1d83a6ca
Mateus Nasc <105395333+Mateus-Nasc@users.noreply.github.com>
2026-05-26 23:29:30 -0300

    feat: implementando typeorm e configurando variaveis de ambiente

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { TarefasModule } from './tarefas/tarefas.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuariosModule, TarefasModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsuariosModule,
+    TarefasModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

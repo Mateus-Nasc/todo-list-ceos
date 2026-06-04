1be6b1556c9433194a0efae6e966ad6ff4d9809a
Mateus Nasc <105395333+Mateus-Nasc@users.noreply.github.com>
2026-06-04 13:01:56 -0300

    infra: inclui 0.0.0.0 no host do listen para suportar o Docker

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     allowedHeaders: '*',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 bootstrap();
